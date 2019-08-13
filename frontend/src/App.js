ba2478380948f0924c883579999bb2b442a80ca1
Treagan Birbal <treaganbirbal@pursuit.org>
2019-08-13 18:51:42 -0400

    implementing network request to get all users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from 'axios';
 
 import LandingPage from "./components/landing_page";
 
-function App() {
+class App extends React.Component {
+  
+  componentDidMount(){
+    axios.get('/users')
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => console.log(err))
+  }
+  
+  render(){
   return (
     <div className="App">
       <LandingPage />
     </div>
   );
+  }
 }
 
 export default App;
