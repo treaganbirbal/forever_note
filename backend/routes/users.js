79c347c8ee4d355edfb8a69ff4f42f5722d86383
Treagan Birbal <treaganbirbal@pursuit.org>
2019-08-13 17:02:01 -0400

    user route created

---
+++ backend/routes/users.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const db = require("../db/queries/usersqueries")
+const passport = require("../auth/local")
+const { loginRequire } = require('../auth/helpers')
+const {getAllUsers, getSingleUser, updateUser, createUser, deleteUser, isLoggedIn, loginUser, logoutUser} = require('../db/queries/usersqueries.js');
+
+
+router.get('/', getAllUsers);
+router.get('/getUser/:id', getSingleUser);
+router.post('/login', passport.authenticate("local", {}), db.loginUser);
+router.get('/isLoggedIn', db.isLoggedIn);
+router.get('/logout', loginRequire, db.logoutUser);
+router.patch('/:id', updateUser);
+router.post('/register', createUser);
+router.delete('/:id', deleteUser);
+
+
+module.exports = router;
